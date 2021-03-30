28e5b2a6e52c3e7144b31211f3cb40d6f28c593b
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2021-03-30 13:09:23 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import * as ROUTES from "./constants/routes";
 
-function App() {
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const Profile = lazy(() => import("./pages/profile"));
+const Project = lazy(() => import("./pages/project"));
+const NotFound = lazy(() => import("./pages/not-found"));
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Suspense
+        fallback={
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+              <div class="flex flex-col text-center mb-12">
+                <img src="/images/loader.gif" alt="loader" />
+              </div>
+            </div>
+          </section>
+        }
+      >
+        <Switch>
+          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route path={ROUTES.PROJECT} component={Project} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router >
   );
 }
-
-export default App;
---
+++ src/constants/routes.js
@@ -0,0 +1,4 @@
+export const DASHBOARD = "/";
+export const PROFILE = "/profile";
+export const PROJECT = "/p/:projectname";
+export const NOT_FOUND = "/not-found";
