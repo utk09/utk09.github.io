6c38ffcce5aca1f792adad57f1741aa685ea3cda
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2021-04-24 23:30:10 +0530

    removed third col

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,18 +6,19 @@ const Experience = () => {
       <div className="bg-white">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
           <div>
-            <h2 className="text-base font-semibold text-indigo-600 uppercase tracking-wide">Everything you need</h2>
-            <p className="mt-2 text-3xl font-extrabold text-gray-900">All-in-one platform</p>
-            <p className="mt-4 text-lg text-gray-500">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>
+            <h2 className="text-base font-semibold text-indigo-600 uppercase tracking-wide">WORK EXPERIENCE</h2>
+            <p className="mt-2 text-3xl font-extrabold text-gray-900">Jio Platforms Limited</p>
+            <p className="mt-4 text-lg text-gray-500">Software Development Engineer</p>
+            <p className="mt-4 text-lg text-gray-500">Analytics COE</p>
           </div>
           <div className="mt-12 lg:mt-0 lg:col-span-2">
             <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-4 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
 
               <div className="relative">
                 <dt>
-                  <svg className="absolute h-6 w-6 text-green-500" data-todo-x-description="Heroicon name: outline/check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  {/* <svg className="absolute h-6 w-6 text-green-500" data-todo-x-description="Heroicon name: outline/check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-                  </svg>
+                  </svg> */}
                   <p className="ml-9 text-lg leading-6 font-medium text-gray-900">Invite team members</p>
                 </dt>
                 <dd className="mt-2 ml-9 text-base text-gray-500">
@@ -61,7 +62,7 @@ const Experience = () => {
               </dd>
               </div>
 
-              <div className="relative">
+              {/* <div className="relative">
                 <dt>
                   <svg className="absolute h-6 w-6 text-green-500" data-todo-x-description="Heroicon name: outline/check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
@@ -107,7 +108,7 @@ const Experience = () => {
                 <dd className="mt-2 ml-9 text-base text-gray-500">
                   Find what you need with advanced filters, bulk actions, and quick views.
               </dd>
-              </div>
+              </div> */}
 
             </dl>
           </div>
