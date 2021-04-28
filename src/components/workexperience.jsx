ebe6c6bbc0de1872a8bf49c84b863a011b965ea1
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2021-04-28 09:14:08 +0530

    removed 2 grid rows

--- src/components/workexperience.jsx
+++ src/components/workexperience.jsx
@@ -12,7 +12,7 @@ const WorkExperience = () => {
             <p className="mt-1 text-lg text-gray-500">Analytics COE</p>
           </div>
           <div className="mt-12 lg:mt-0 lg:col-span-2">
-            <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-rows-4 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
+            <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-rows-2 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
 
               <div className="relative">
                 <dt>
@@ -26,24 +26,6 @@ const WorkExperience = () => {
               <div className="relative">
                 <dt>
                   <p className=" text-lg leading-6 font-medium text-gray-900">List view</p>
-                </dt>
-                <dd className="mt-2  text-base text-gray-500">
-                  You can manage phone, email and chat conversations all from a single mailbox.
-              </dd>
-              </div>
-
-              <div className="relative">
-                <dt>
-                  <p className=" text-lg leading-6 font-medium text-gray-900">Keyboard shortcuts</p>
-                </dt>
-                <dd className="mt-2  text-base text-gray-500">
-                  You can manage phone, email and chat conversations all from a single mailbox.
-              </dd>
-              </div>
-
-              <div className="relative">
-                <dt>
-                  <p className=" text-lg leading-6 font-medium text-gray-900">Calendars</p>
                 </dt>
                 <dd className="mt-2  text-base text-gray-500">
                   You can manage phone, email and chat conversations all from a single mailbox.
