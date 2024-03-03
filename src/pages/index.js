41a5727ff458e7066f4fb04730f7e506d40bc674
utk09 <utk0997@gmail.com>
2024-03-03 03:09:01 +0000

    added cronitor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import * as Cronitor from "@cronitorio/cronitor-rum";
 import HomepageHeader from "@site/src/components/HomepageHeader";
 import HomepageContent from "@site/src/components/HomepageContent";
 import CustomFooter from "@site/src/components/CustomFooter";
 
 export default function Home() {
+  React.useEffect(() => {
+    // Load the Cronitor tracker once in your app
+    Cronitor.load("145f029f6766bab393d89fc3b444d4c4");
+    Cronitor.track("PageView");
+  }, []);
+
   return (
     <>
       <HomepageHeader />
