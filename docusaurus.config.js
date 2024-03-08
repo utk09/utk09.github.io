5348643a76e59a0412413110332a650eb0ccd569
utk09 <utk0997@gmail.com>
2024-03-08 21:09:08 +0000

    updated sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
         sitemap: {
           changefreq: "weekly",
           priority: 0.9,
-          ignorePatterns: ["/tags/**"],
+          // ignorePatterns: ["/tags/**"],
           filename: "sitemap.xml",
         },
         gtag: {
